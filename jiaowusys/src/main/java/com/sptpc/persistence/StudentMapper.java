126402a5755a9b418c137fed687b9444d64f324f
firstdeng <1501319665@qq.com>
2018-04-19 16:59:44 +0800

    2018.4.19

---
+++ jiaowusys/src/main/java/com/sptpc/persistence/StudentMapper.java
@@ -0,0 +1,35 @@
+package com.sptpc.persistence;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.sptpc.domain.Student;
+
+@Repository
+public interface StudentMapper {
+	public List<Student> getAllStudent();
+	
+	//插入学生信息
+	public int insertStudent(Student student);
+
+	//根据ID找学生
+	public Student findStudentByID(int id);
+
+	//更新学生信息
+
+	public int updateStudent(Student student);
+
+		//删除学生信息
+	public int deleteStudentByID(int id);
+	
+	//根据学生id删除学生信息
+	
+	public int deleteStuCourseByID(int id);
+
+	
+}
+
+
+
+
